fdc066ac5cfb54e2b934a71385a7c07150b697fe
newswim <ofenixculpa@gmail.com>
2017-02-15 14:52:18 -0600

    Add notes for 1.1 and how-to, begin 1.2 notes

---
+++ python_aka_composing_programs/1.1/readme.md
@@ -0,0 +1,16 @@
+[1.1 - Getting Started](http://composingprograms.com/pages/11-getting-started.html)
+
+### Topics
+
+- Import statement
+- Data and Functions
+- Expressions and Statements
+- Interactive Interpretor (shell)
+- Zen of Python
+- Installation instructions
+- `Set` (type of data structure, more in Chap 2)
+- Error handling
+  - Test incrementally
+  - Isolate errors
+  - Check your assumptions
+  - Consult others
---
+++ python_aka_composing_programs/1.1/shakespeare.py
@@ -0,0 +1,15 @@
+'''
+is an import statement that loads functionality for accessing data on the
+Internet. In particular, it makes available a function called urlopen, which
+can access the content at a uniform resource locator (URL), a location of
+something on the Internet.
+'''
+from urllib.request import urlopen
+
+shakespeare = urlopen('http://composingprograms.com/shakespeare.txt')
+
+words = set(shakespeare.read().decode().split())
+
+test = {w for w in words if len(w) == 6 and w[::-1] in words}
+
+print(test)
